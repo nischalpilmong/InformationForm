a7e8d34483831153b7754ac21718407e0b7c984d
Nischal Rai <nischalpilmong@yahoo.com>
2020-03-31 18:19:12 +0200

    Final commit

--- js/validation.js
+++ js/validation.js
@@ -1,8 +1,15 @@
 function validateForm(){
-  for(var i = 0; i < infoForm.elements.length; i++){
-    if(infoForm.elements[i].className == "req" && infoForm.elements[i].value.length == 0){
-      alert('Please fill in all required fields');
-      return false;
-    }
-  }
+   for(var i = 0; i < infoForm.elements.length; i++){
+     if(infoForm.elements[i].className == 'req' && infoForm.elements[i].value.length == 0){
+       alert('Please fill in all required fields');
+       return false;
+     }
+   }
+   var email = document.getElementById('email').value;
+   var atpos = email.indexOf('@');
+   var dotpos = email.lastIndexOf('.');
+   if(atpos < 1 || dotpos < atpos + 2 || dotpos+2 >= x.length){
+     alert('Please type a valid email address');
+     return false;
+   }
 }
